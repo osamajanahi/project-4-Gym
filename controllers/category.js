fc18ae09510b1c4a66e5d078fcf3baac5caf7df0
osamajanahi <osamaj12@gmail.com>
2024-01-10 21:26:40 +0300

    Done with category

--- controllers/category.js
+++ controllers/category.js
@@ -1,1 +1,43 @@
 const {Category} = require('../models/Category');
+
+exports.category_create_post = (req, res) =>{
+    let cate = new Category(req.body)
+    cate.save()
+    .then(newCate =>{
+        console.log("Added successfully");
+        res.json(newCate);
+    })
+    .catch(err =>{
+        console.log(err)
+    })
+}
+
+exports.category_index_get = (req, res) =>{
+    Category.find()
+    .then(category =>{
+        res.json(category);
+    })
+    .catch(err =>{
+        console.log(err);
+    })
+}
+
+exports.category_edit_get = (req, res) =>{
+    Category.findById(req.query.id)
+    .then(oneCat =>{
+        res.json(oneCat);
+    })
+    .catch(err =>{
+        console.log(err);
+    })
+}
+
+exports.category_edit_post = (req, res) =>{
+    Category.findByIdAndUpdate(req.body.id, req.body, { new: true })
+    .then(updatedCat =>{
+        res.json(updatedCat);
+    })
+    .catch(err =>{
+        console.log(err);
+    })
+}
